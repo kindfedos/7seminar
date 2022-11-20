1e8a0faab44c9855657842aa4cc134a9f556a6eb
Fedor <kurnosenkovf@mail.ru>
2022-11-20 12:30:09 +0400

    47 task

---
+++ 47/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.WriteLine("введите количество строк");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int l = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[k,l];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
